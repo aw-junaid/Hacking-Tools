c6aad13ad9139082fb69a341bea450317dc9faef
aw-junaid <abdulwahabjunaid07@gmail.com>
2024-10-21 18:57:44 +0500

    commit

--- Nessus Attack Scripting Language/openvas_get_version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import gvm
-from gvm.protocols.latest import Gmp
-
-connection = gvm.connections.TLSConnection(hostname='localhost')
-with Gmp(connection=connection) as gmp:
-    version = gmp.get_version()
-    print(version)
